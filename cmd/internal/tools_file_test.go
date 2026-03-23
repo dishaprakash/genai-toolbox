a6c368b7997e0f0e572bc65ebdca77b88ec5c2a1
duwenxin <duwenxin@google.com>
2026-03-23 15:28:34 -0400

    nit

--- cmd/internal/tools_file_test.go
+++ cmd/internal/tools_file_test.go
@@ -694,7 +694,7 @@ func TestParseToolFile(t *testing.T) {
 						Type:                   generic.AuthServiceType,
 						Audience:               "testings",
 						McpEnabled:             true,
-						AuthorizationServerURL: "https://testings",
+						AuthorizationServerUrl: "https://testings",
 						ScopesRequired:         []string{"read:files", "write:files"},
 					},
 				},
